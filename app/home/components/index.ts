dccb4941ff0ad5ff8b92eb601ba6888e2af3ab7f
Gabriel P. <cgabrielpimentacruz111@gmail.com>
2026-04-15 15:56:11 -0300

    feat: Create index file to export all section components

---
+++ app/home/components/index.ts
@@ -0,0 +1,8 @@
+export { Features } from "./sections/Features";
+export { FinalCta } from "./sections/FinalCta";
+export { Footer } from "./sections/Footer";
+export { Header } from "./sections/Header";
+export { Hero } from "./sections/Hero";
+export { Pricing } from "./sections/Pricing";
+export { Steps } from "./sections/Steps";
+export { Testimonials } from "./sections/Testimonials";
